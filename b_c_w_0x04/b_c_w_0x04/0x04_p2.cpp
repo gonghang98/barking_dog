5444b9f4b02c8fde8556b341e99fd773b3d8bd7b
gonghang98 <speed3998@dankook.ac.kr>
2025-07-20 00:18:40 +0900

    연결 리스트 벽느낌

--- b_c_w_0x04/b_c_w_0x04/0x04_p2.cpp
+++ b_c_w_0x04/b_c_w_0x04/0x04_p2.cpp
@@ -1,3 +1,4 @@
+//¹Ì¿Ï
 #include <algorithm>
 #include <iostream>
 #include <string>
@@ -6,42 +7,43 @@ using namespace std;
 
 int main(void) {
 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int n = 0;
     char order = 0, p_char = 0;
-    int unused = 0;
-    list<char> string_List;
+    list<char> string_list;
     string str1;
     cin >> str1;
-    string_List.insert(string_List.begin(), str1);
-    
+    for (auto c : str1) string_list.push_back(c);
+    auto cursor = string_list.end();
     cin >> n;
-    unused = str1.length();
+
     //cout << unused;
-    for (int i = 0; i < n; i++)
+    while (n--)
     {
         cin >> order;
-        switch (order) {
-        case 'L':
-            unused--;
-            break;
-
-        case 'D':
-            unused++;
-            break;
-
-        case 'B':
-            
-            break;
-
-        case 'P':
-
-            break;
-        default:
-            break;
+        if (p_char == 'P')
+        {
+            cin >> p_char;
+            string_list.insert(cursor, p_char);
         }
-
+        else if (p_char == 'L') 
+        {
+            if (cursor != string_list.begin()) cursor--;
+        }
+        else if (p_char == 'D')
+        {
+            if (cursor != string_list.end()) cursor++;
+        }
+        else
+        {
+            if (cursor != string_list.begin()) {
+                cursor--;
+                cursor = string_list.erase(cursor);
+            }
+        }
     }
-
-    cout << unused;
+        for (auto c : string_list) cout << c;
 
 }
