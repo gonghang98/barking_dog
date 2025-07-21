39d7cf53fb18d5e0fad2a22d2c5e7ce722aa3799
gonghang98 <speed3998@dankook.ac.kr>
2025-07-21 17:54:25 +0900

    스택

---
+++ 0x05/0x05/0x05_1.cpp
@@ -0,0 +1,46 @@
+//10828	
+#include <iostream>
+#include <stack>
+#include<string>
+using namespace std;
+
+int main(void) 
+{
+	stack<int> S;
+	int n, m;
+	string str;
+	cin >> n;
+	while (n--)
+	{
+		cin >> str;
+		if (str == "push")
+		{
+			cin >> m;
+			S.push(m);
+		}
+		else if (str == "pop")
+		{
+			if (S.empty()) cout << -1 <<'\n';
+			else
+			{
+				cout << S.top() << '\n';
+				S.pop();
+			}
+		}
+		else if (str == "size") cout << S.size() << '\n';
+		else if (str == "empty")
+		{
+			if (S.empty()) cout << 1 << '\n';
+			else cout << 0 << '\n';
+		}
+		else
+		{
+			if (S.empty()) cout << -1 << '\n';
+			else cout << S.top() << '\n';
+		}
+
+	}
+
+	return 0;
+
+}
